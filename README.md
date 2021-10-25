5b33ce5cc47ef7a10d664cb9d83a0233fc47980d
Laurence Pakenham-Smith <laurence@sourceless.org>
2021-10-25 19:45:34 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# wizard
+Wizard is my spare time programming language project. It's paired with another
+language, Warlock, which will compile to Wizard.
+
+# Aims
+Create a lisp that is:
+* Quantatively typed
+* Compiled (to C or LLVM IR)
+* Fully memory-safe
+* Pure
+
+In order to create a small language that can be built upon and used as a target
+for higher level languages.
+
+More than anything, I want to bring dependent types to more people, and improve
+our programs just a little bit.
+
+# Inspiration
+Wizard owes its design and principles to many other projects; here are just a
+few.
+
+## Rust
+Rust has fantastic memory safety and lifecycle management, which is something
+that I would like Wizard to emulate. In addition, Rust's packaging story is
+possibly the best out there at the time of writing (2021).
+
+However, Rust could have gone further with its type system and focuses mainly
+on system programming as a primary usecase. This is great, but I want something
+with a better type system and a bit more high-level.
+
+## Clojure
+Clojure's immutability and lisp syntax makes it the top build-your-own-language
+out there today, other than perhaps Racket.
+
+I think, however, it is suffering the same fate as many dynamically typed
+languages, where you can only gain so much confidence in the implementation
+from static analysis and runtime checking.
+
+## Python
+Python's batteries-included standard libraries and approachable nature make it
+a default choice for me on some projects. Its strong insistence to stick to
+its principles have allowed it to stand the test of time and become something
+of a lingua franca.
+
+However, it falls foul of many of the same issues as clojure, but it's also
+objectively a less powerful language due to its lack of macros and some of the
+early design choices which still plague the runtime. It's also not great to
+distribute.
+
+## Idris
+Idris, and Idris 2 specifically, is the first real pacman-complete usage of
+dependent types that I have seen.
+
+However, its adherence to the ML-style syntax is, I think, a downside, which
+makes it less approachable to those not already in the Haskell-sphere.
---
+++ src/wizard.clj
@@ -0,0 +1,82 @@
+(ns wizard)
+
+(def wizard-example
+  '((type inc (int -> int))
+    (defn inc
+      ([n]
+       (+ n 1)))))
+
+
+; so to get types to work i have to understand wtf a type is
+; so there's a few possible angles here
+; a type is:
+;   -- primitive types
+;   - some builtin concept, i.e. a primitive type, e.g. an int
+;   - a type with a single constructor used for creating trivially isomorphic types
+;
+;   -- algebraic types
+;   - a sum type, i.e. a choice between 2 (or more) types
+;   - a product type, i.e. a tuple of types
+;
+;   -- dependent/first-class types
+;   - a dependent function type, i.e. a family of types that vary according to some value,
+;     essentially a cartesian product of types. This means we can define, for example,
+;     *vectors*, which are lists with a length specified in their type 0-vectors,
+;     1-vectors, and 2-vectors are distinct types and we should not expect to be
+;     able to necessarily compare them!
+;   - a dependent sum type, which corresponds to an existential quantifier; this means
+;     that we can define types where the type of the second element of a pair is
+;     dependent upon the type of the first element of a pair
+;
+; so a few builtin types would be good;
+;   - Int
+;   - String
+;   - Bool
+;   - Unit (the type with only one value)
+;   - Void (the type with no values)
+
+(defn eval-defs [env body]
+  (let [name (first body)
+        sig (rest body)]
+    (assoc-in env [:types name] sig)))
+
+(defn eval-defn [env body])
+
+(def builtins
+  {'defs eval-defs
+   'defn eval-defn
+   '+ +})
+
+(defn eval-form [form env]
+  (let [f (get (:functions env) (first form))]
+    (f env (rest form))))
+
+; When we eval we should actually do a few things first
+;   1. type check
+;        iterate over all the type and function definitions in the program
+;        and check that they are logically sound. if that's good, then we
+;        can eval knowing the program is typesafe
+;   2. actually that's it, the second step is to run the eval
+;   3. oh actually tbf we can pretty much erase types to some degree after
+;        we're sure that the program typechecks, i.e. we don't need to keep
+;        the typedefs and signatures around anymore, we just need to have
+;        the bits of the program that do stuff
+;   4. if we were compiling there would be a bunch more here but since we're
+;        interpreting and abusing the shit out of having a host language with
+;        a nice library of functions, we can avoid this for now!
+(defn eval-wizard [program]
+  (reduce eval-form initial-env program))
+
+(defn typecheck-form [form env]
+  )
+
+(defn typecheck [program]
+  "Type checks a wizard program"
+  (reduce typecheck-form initial-env program))
+
+(def initial-env
+  {:types {}
+   :functions builtins})
+
+(eval-form '(defs foo (a -> a -> a)) initial-env)
+; (eval-wizard wizard-example)
