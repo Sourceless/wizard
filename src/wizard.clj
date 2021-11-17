ea37ca244894090b55bbd8b0b67d6136bffad013
Laurence Pakenham-Smith <laurence@sourceless.org>
2021-11-17 11:39:14 +0000

    Relatively complete type parsing, typechecking of simple types

--- src/wizard.clj
+++ src/wizard.clj
@@ -1,12 +1,5 @@
 (ns wizard)
 
-(def wizard-example
-  '((type inc (int -> int))
-    (defn inc
-      ([n]
-       (+ n 1)))))
-
-
 ; so to get types to work i have to understand wtf a type is
 ; so there's a few possible angles here
 ; a type is:
@@ -38,10 +31,14 @@
 
 (defrecord Type [name terms type constructors])
 (defrecord Term [name multiplicity type])
-(defrecord Constructor [name terms type])
+(defrecord Constructor [name arguments type])
+(defrecord FunctionType [arguments return-type])
+(defrecord Value [type data])
 
 (defn reference-type [name env]
   (let [type-name (keyword "type" (str name))]
+    (prn type-name)
+    (prn (keys (:types env)))
     (if (contains? (:types env) type-name)
       type-name
       (throw (Exception. (str "No type known named " type-name))))))
@@ -57,40 +54,51 @@
       (keyword term)
       (throw (Exception. (str "Unexpected term " term " in constructor of type " (:name typedef) ". Valid terms: " (:terms typedef)))))))
 
+(defn is-term [term]
+  (every? #(Character/isLowerCase %) (str term)))
+
+(defn is-own-type [term typedef]
+  (= (str term) (:name typedef)))
+
+(defn is-known-type [term env]
+  (contains? (:types env) (keyword "type" (str term))))
+
 (defn parse-constructor-term [term typedef env]
   (cond
-    (every? #(Character/isLowerCase %) (str term)) (term-if-exists term typedef)
-    :else (throw (Exception. (str "Unknown constructor term encountered: " term)))))
+    (seq? term) (map #(parse-constructor-term % typedef env) term)
+    (is-term term) (term-if-exists term typedef)
+    (is-own-type term typedef) (keyword "type" (:name typedef))
+    (is-known-type term env) (keyword "type" (str term))
+    :else (throw (Exception. (str "No matching term or type for constructor argument: " term)))))
 
 (defn list-terms [typedef]
   (let [terms (vals (:terms typedef))]
-    (prn terms)
     (map #(symbol (:name %)) terms)))
 
 (defn typedef-to-basic-type [typedef]
-  (prn typedef)
-  (prn (list-terms typedef))
-  (conj (list-terms typedef) (symbol (:name typedef))))
+  (conj (list-terms typedef) (:name typedef)))
 
-(defn constructor-type [constructor-terms typedef]
-  "To be implemented")
+;; (defn parse-constructor-arg [arg typedef]
+;;   (cond
+;;     (is-term arg) (arg (:terms typedef))
+;;     (is-type arg) ))
 
-(defn parse-sum-constructor [spec typedef env]
+(defn constructor-type [constructor-args typedef]
+  (FunctionType. constructor-args (keyword "type" (:name typedef))))
+
+(defn parse-constructor [spec typedef env]
   (if (seq? spec)
     (let [name (first spec)
           terms (rest spec)
           constructor-terms (map #(parse-constructor-term % typedef env) terms)]
       (Constructor. name constructor-terms (constructor-type constructor-terms typedef)))
-    (Constructor. spec nil (typedef-to-basic-type typedef))))
+    (Constructor. spec nil (:name typedef))))
 
 (defn parse-sum-constructors [spec typedef env]
-  (prn spec)
-  (map #(parse-sum-constructor %1 typedef env) spec))
+  (map #(parse-constructor %1 typedef env) spec))
 
 (defn parse-data-constructors [spec typedef env]
-  (cond
-    (= (first spec) '+) (parse-sum-constructors (rest spec) typedef env)
-    :else (throw (Exception. "Not implemented"))))
+    (parse-sum-constructors spec typedef env))
 
 (defn parse-data-name [name-and-terms]
   (if (seq? name-and-terms)
@@ -130,14 +138,60 @@
         constructors (parse-data-constructors (first spec) (Type. (str name) terms type' nil) env)]
     (Type. (str name) terms type' constructors)))
 
+(defn assertion-name [form]
+  (let [name (str (second form))]
+    (cond
+      (is-term name) (keyword name)
+      :else (keyword "type" name))))
+
+(defn parse-function-type [signature]
+  "not implemented")
+
+(defn parse-type [signature]
+  "also not implemented")
+
+(defn assertion-type [form env]
+  (let [signature (last form)]
+    (cond
+      (seq? signature) (cond
+                         (= '-> (first signature)) (parse-function-type signature)
+                         :else (parse-type signature))
+      :else (reference-type signature env))))
+
+(defn typeof-assertion [name env]
+  (if (contains? (:assertions env) name)
+    (name (:assertions env))
+    (throw (Exception. (str "No type assertion for " name)))))
+
+(defn typecheck-def [form env]
+  (let [name (assertion-name form)
+        value (last form)
+        assertion (typeof-assertion name env)
+        value-type (:type value)]
+    (if (= assertion value-type)
+      env
+      (throw (Exception. (str "Type mistmatch: " name " should be a " assertion " but is a " value-type))))))
+
+(defn add-constructor [env constructor]
+  ; reduce over the constructors
+  env
+  )
+
+(defn add-constructors [env t]
+  env)
+
 (defn typecheck-form [env form]
-  (if (= 'data (first form))
-    (let [t (parse-data form env)]
-      (if (contains? (:types env) (:name t))
-        (throw (Exception. (str "Type " (:name t) " already exists.")))
-        nil)
-      (assoc-in env [:types (:name t)] t))
-    env))
+  (cond
+    (= 'data (first form)) (let [t (parse-data form env)]
+                             (if (contains? (:types env) (:name t))
+                               (throw (Exception. (str "Type " (:name t) " already exists.")))
+                               nil)
+                             (let [env-with-type (assoc-in env [:types (keyword "type" (:name t))] t)
+                                   env-with-type-and-constructors (add-constructors env t)]
+                               env-with-type-and-constructors))
+    (= 'type (first form)) (assoc-in env [:assertions (assertion-name form)] (assertion-type form env))
+    (= 'def (first form)) (typecheck-def form env)
+    :else env))
 
 (defn typecheck [env program]
   (reduce typecheck-form env program))
@@ -145,43 +199,79 @@
 ; So I revised the syntax from the ML-style to something lispier
 ;
 ; Quick guide:
-;   - Int, a, etc.: 'simple' types or terms
+;   - Int, a, etc.: types and terms
+;   - (-> a b): a function type from a to b
 ;   - (= x a): named term with explicit type
 ;   - (== 1 x a): named term with explicit multiplicity
-;   - (+ a a): sum type
-;   - (* a a): product type
-(def wizard-types-example
-  ; 'data' introduces a new type into the program
-  '((data MyInt Int) ; a type alias
-    (data Pair (* Int Int)) ; a product type
-    (data Choice (+ Int String)) ; a sum type
-    (data Bool (+ True False)) ; a bool (sum type with two nullary constructors)
-    (data IntList [Int]) ; a list of Ints
-    (data (List a) (+ Nil (Cons a (List a)))) ; a simple homogeneous list
-    (data OtherList (-> (= a Type) Type) ; Same list, different syntax
-          (where [Nil (List a)
-                  "::" (-> a (List a) (List a))]))
-    (data (Tree a) (+ (Leaf a) (Node (Tree a) (Tree a)))) ; a simple homogeneous tree
-    (data Nat (+ Zero (S Nat))) ; a natural number, peano-style
-    (data OtherNat Type ; a natural number using the longer syntax
-          (where [Zero Nat
-                  S (-> Nat Nat)]))
-    (data Vect (-> (= n Nat) a Type) ; a more complex dependent type signature
-          (where [Nil (Vect 0 a)
-                  "::" (-> (= x a) (= xs (Vect n a)) (Vect (S n) a))]))
+;; (def wizard-types-example
+;;   ; 'data' introduces a new type into the program
+;;   '((data MyInt Int) ; a type alias
+;;     (data Pair [(Pair Int Int)]) ; a product type
+;;     (data Choice [Int String]) ; a sum type
+;;     (data Bool [True False]) ; a bool (sum type with two nullary constructors)
+;;     (data (List a) [Nil (Cons a (List a))]) ; a simple homogeneous list
+;;     (data IntList [(List Int)] ; a list of Ints
+;;     (data OtherList (-> (= a Type) Type) ; Same list, different syntax
+;;           (where [Nil (List a)
+;;                   "::" (-> a (List a) (List a))]))
+;;     (data (Tree a) (+ (Leaf a) (Node (Tree a) (Tree a)))) ; a simple homogeneous tree
+;;     (data Nat [Zero
+;;                (S Nat)]) ; a natural number, peano-style
+;;     (data OtherNat Type ; a natural number using the longer syntax
+;;           [(Zero Nat)
+;;            (S (-> Nat Nat))])
+;;     (data Vect (-> (= n Nat) a Type) ; a more complex dependent type signature
+;;           [(Nil (Vect 0 a))
+;;            ("::" (-> (= x a) (= xs (Vect n a)) (Vect (S n) a)))])
 
-    ; 'type' asserts that a term with a certain name has a certain type
-    (type a-number Int) ; I have a constant named 'a-number' which is an Int
-    (type inc (-> Int Int)) ; the type signature of a function that increments an int
-    (type curried (-> Int Int Int)) ; the type signature of a function with more than one arg
-    ))
+;;     ; 'type' asserts that a term with a certain name has a certain type
+;;     (type a-number Int) ; I have a constant named 'a-number' which is an Int
+;;     (type inc (-> Int Int)) ; the type signature of a function that increments an int
+;;     (type curried (-> Int Int Int)) ; the type signature of a function with more than one arg
+;;     )))
 
 (def initial-env
-  {:types {:type/Type (Type. "Type" nil :type/Type nil)}})
+  {:types {:type/Type (Type. "Type" nil :type/Type nil)
+           :type/Int (Type. "Int" nil :type/Type nil)
+           :type/String (Type. "String" nil :type/Type nil)}
+   :assertions {}
+   :type-constructors {}})
 
-; (typecheck {:types {}} wizard-types-example)
+(def my-little-program '(
+    (data (Maybe a) [(Just a)
+                     Nothing])
+    (data Point [(Point Int Int)])
+    (data (List a) [Nil
+                    (Cons a (List a))])
 
-(typecheck initial-env '((data (Maybe a) (+ (Just a) Nothing))))
+    (type Point Type)
 
-;; (typecheck {:types {}} '((data MyInt Int)
-;;                          (data Pair (Int * Int))))
+    (type x Int)
+    (def x 0)
+
+    (type y String)
+    (def y "Hello")
+
+    (type z Point)
+    (def z (Point 1 2))
+
+    (type a (Maybe Int))
+    (def a (Just 1))
+))
+
+
+(defn parse-primitive [form]
+  (cond (seq? form) (map parse-primitive form)
+        (string? form) (Value. :type/String form)
+        (int? form) (Value. :type/Int form)
+        :else form))
+
+(defn parse-primitives [program]
+  (map parse-primitive program))
+
+;(parse-primitives '((def x 1)))
+;(typecheck initial-env my-little-program)
+
+(->> my-little-program
+     parse-primitives
+     (typecheck initial-env))
